5bbade68bb592006562edfda7f7efe1c691766cb
PawanMali <51850335+PawanMali@users.noreply.github.com>
2019-07-18 13:55:11 +0530

    MasterUpload

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+// dbPassword = 'mongodb+srv://USERNAME_HERE:'+ encodeURIComponent('PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+
+module.exports = {
+    mongoURI: 'mongodb+srv://pawan:testdb@clustercube0-kftti.mongodb.net/test?retryWrites=true&w=majority'
+};
